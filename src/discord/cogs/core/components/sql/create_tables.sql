ecf216c9d7c9c2f8a362be7783c147f6f75f8c06
RogueWolf95 <139587638+RogueWolf95@users.noreply.github.com>
2023-07-29 19:51:49 +0000

    SQL tables

--- src/discord/cogs/core/components/sql/create_tables.sql
+++ src/discord/cogs/core/components/sql/create_tables.sql
@@ -14,4 +14,32 @@ CREATE TABLE Recipe (
     description TEXT,
     rating REAL
 );
+
+-- Ingredient Table
+CREATE TABLE Ingredient (
+    ingredient_id INTEGER PRIMARY KEY,
+    name TEXT,
+    category TEXT
+);
+
+-- Step Table
+CREATE TABLE Step (
+    step_id INTEGER PRIMARY KEY,
+    recipe_id INTEGER,
+    step_number INTEGER,
+    instruction TEXT,
+    FOREIGN KEY (recipe_id) REFERENCES Recipe (recipe_id)
+);
+
+
+-- Rating Table
+CREATE TABLE Like (
+    like_id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    recipe_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES User (user_id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe (recipe_id)
+);
+
+
     
