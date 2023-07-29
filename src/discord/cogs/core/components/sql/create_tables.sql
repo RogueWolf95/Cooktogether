d7e7f8009a787cbbb3ac3612250384b0127939ca
RogueWolf95 <139587638+RogueWolf95@users.noreply.github.com>
2023-07-29 19:07:11 +0000

    SQL Tables

--- src/discord/cogs/core/components/sql/create_tables.sql
+++ src/discord/cogs/core/components/sql/create_tables.sql
@@ -1,33 +1,17 @@
-
--- Create table "User"
-CREATE TABLE IF NOT EXISTS User(
-    DUID INTEGER PRIMARY KEY,
-    Handle TEXT NOT NULL,
-    RegDate DATE NOT NULL,
-    Email TEXT,
-    isOver21 BOOLEAN
+--User Table
+CREATE TABLE User (
+    user_id INTEGER PRIMARY KEY, 
+    username TEXT,
+    discord_user_id TEXT UNIQUE,
+    avater_url TEXT
 );
 
--- Create table "Ingredient"
-CREATE TABLE IF NOT EXISTS Ingredient(
-    IngredientID INTEGER PRIMARY KEY,
-    IngredientName TEXT
+--Recipe Table
+CREATE TABLE Recipe (
+    recipe_id INTEGER PRIMARY KEY,
+    title TEXT,
+    image_url TEXT,
+    description TEXT,
+    rating REAL
 );
-
--- Create table "Ingredient"
-CREATE TABLE IF NOT EXISTS Recipe(
-    RecipeID INTEGER PRIMARY KEY,
-    RecipeName TEXT,
-    RecipeDesc TEXT,
-    hasAlchohol BOOLEAN
-);
-
--- Create table "RecipeIngredient"
-CREATE TABLE IF NOT EXISTS RecipeIngredient(
-    RecipeID INTEGER,
-    IngredientID INTEGER,
-    Measurement TEXT,
-    PRIMARY KEY (RecipeID, IngredientID),
-    FOREIGN KEY (RecipeID) REFERENCES Recipe(RecipeID),
-    FOREIGN KEY (IngredientID) REFERENCES Ingredient(IngredientID)
-);
+    
